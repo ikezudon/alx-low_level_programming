8f4152f4bf2a2d8bae3b597e80075e32525bd2b2
Ikenna Nwachukwu <ikezudon@gmail.com>
2022-06-07 15:02:16 -0700

    8-24_hours.c

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -16,7 +16,8 @@ j = 0;
 while (j < 60)
 {
 _putchar((i / 10) + '0');
-_putchar((i % 10) + '0');_putchar(':');
+_putchar((i % 10) + '0');
+_putchar(':');
 _putchar((j / 10) + '0');
 _putchar((j % 10) + '0');
 _putchar('\n');
