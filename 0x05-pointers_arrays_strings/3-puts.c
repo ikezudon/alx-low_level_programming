22b767485f1c610756d3dabef7cde60e67fe16ab
Ikenna Nwachukwu <ikezudon@gmail.com>
2022-06-13 13:28:14 -0700

    3-puts.c

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,31 +1,17 @@
 #include "main.h"
 
-
-
 /**
- *
- *  * _puts - prints a string to stdout
- *
- *   * @str: pointer to the string to print
- *
- *    */
+* _puts - prints a string to stdout
+* @str: pointer to the string to print
+*/
 
 void _puts(char *str)
 
 {
-
-		int i;
-
-
-
-			for (i = 0; str[i] != '\0'; i++)
-
-					{
-
-								_putchar(str[i]);
-
-									}
-
-				_putchar('\n');
-
+int i;
+for (i = 0; str[i] != '\0'; i++)
+{
+_putchar(str[i]);
+}
+_putchar('\n');
 }
