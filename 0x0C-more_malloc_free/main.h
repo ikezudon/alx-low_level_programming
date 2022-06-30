5ea5b22ae04cc7b11ee927e102e17ccd30e975d2
Ikenna Nwachukwu <ikezudon@gmail.com>
2022-06-30 09:39:57 -0700

    main.h

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -11,10 +11,5 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
 void errors(void);
 int _strlen(char *s);
 int is_digit(char *s);
-void _print(char *str, int l)
-char *mul(char n, char *num, int num_index, char *dest, int dest_index)
-int check_for_digits(char **av)
-void init(char *str, int l)
-int main(int argc, char *argv[])
   
 #endif
