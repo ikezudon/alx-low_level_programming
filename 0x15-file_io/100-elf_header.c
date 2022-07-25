4518d272d72b93ea4a016f6b05d04b25cde04b71
Ikenna Nwachukwu <ikezudon@gmail.com>
2022-07-25 15:27:55 -0700

    checked for trailing whitespace again

--- 0x15-file_io/100-elf_header.c
+++ 0x15-file_io/100-elf_header.c
@@ -327,7 +327,6 @@ print_osabi(header->e_ident);
 print_abi(header->e_ident);
 print_type(header->e_type, header->e_ident);
 print_entry(header->e_entry, header->e_ident);
-
 free(header);
 close_elf(o);
 return (0);
