04bb9f5fdfa1de0d9bce0ab6ab2d666ba673a3cf
Ikenna Nwachukwu <ikezudon@gmail.com>
2022-09-24 01:35:05 -0700

    removed empty line of code

--- 0x1A-hash_tables/1-djb2.c
+++ 0x1A-hash_tables/1-djb2.c
@@ -1,5 +1,4 @@
 #include "hash_tables.h"
-
 /**
  * hash_djb2 - Hash function implementing the djb2 algorithm.
  * @str: The string to hash.
@@ -10,10 +9,8 @@ unsigned long int hash_djb2(const unsigned char *str)
 {
   unsigned long int hash;
   int c;
-
   hash = 5381;
   while ((c = *str++))
     hash = ((hash << 5) + hash) + c; /* hash * 33 + c */
-
   return (hash);
 }
